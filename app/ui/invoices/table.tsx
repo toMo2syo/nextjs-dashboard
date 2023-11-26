28601fd4078a14ad5815930d53ac19217450b3d1
toMo2syo <qingying54@gmail.com>
2023-11-26 15:30:35 +0800

    fix missing alt on Image component at invoices/table.tsx line 88

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -88,6 +88,7 @@ export default async function InvoicesTable({
                       <Image
                         src={invoice.image_url}
                         className="rounded-full"
+                        alt={`${invoice.name}'s profile picture`}
                         width={28}
                         height={28}
                       />
